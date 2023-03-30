08f261925c96e7e0a5b3e9be7532cc98ef552186
Julian Gomez <juliangomez.xvii@gmail.com>
2023-03-30 13:29:03 -0300

    better modularization for styles of components

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -1,4 +1,4 @@
-import "../styles/styles.css";
+import "../styles/videoplayer.css";
 import React, { useState, useRef } from "react";
 import ReactPlayer from "react-player";
 import Video from "../assets/Video.mp4";
